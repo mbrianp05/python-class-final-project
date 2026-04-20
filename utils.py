62b325c5a52a04a544148836e916b6609d72abb8
Brian <mbrianp05@gmail.com>
2026-04-20 11:58:57 -0400

    Implemented method to verify whether a path is correct or not

--- utils.py
+++ utils.py
@@ -1,5 +1,13 @@
+import os
+
 name_max_len = 14
 
 
 def shorten_gesture_name(name: str) -> str:
     return name[:name_max_len] + "..." if len(name) > name_max_len else name
+
+
+# Verificar que el path exista
+# y que no sea un programa sino una carpeta
+def is_valid_path(path: str) -> bool:
+    return os.path.exists(path) and os.path.isdir(path)
