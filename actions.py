46da62af6a3b476bafb1c9c04fa7a421dc07c7cf
Brian <mbrianp05@gmail.com>
2026-04-20 12:22:29 -0400

    Some refactor and open file function implemented

--- actions.py
+++ actions.py
@@ -1,18 +1,29 @@
 # AQUÍ SE PONDRÁN TODAS LAS FUNCIONES BÁSICAS QUE LOS GESTOS USAN PARA EJECUTARSE
 # DE MOMENTO SE IMPLEMENTARÁN ESTAS, SE PUEDE AÑADIR MÁS IDEAS SE SE DESEA.
 
-import os
+# Aqui no se valida el path sino que asumimos que es correcto
+# el path se valida cuando el usuario lo introduce a la hora de
+# configurar el gesto
+# Hay que tener en cuenta que el usuario podrá introducir el path
+# escribiendolo manualmente, copiandolo o con un botón para navegar de la UI
+# por el sistema de archivos hasta que llegue a la carpeta deseada
+
+import subprocess
 
 
-# Validar que el path sea correcto y si lo es
 # abrir el explorador en esa carpeta
 def open_explorer_at(path: str) -> None:
-    os.startfile(path)
+    subprocess.run(["explorer", path])
 
 
-# Validar el path y abrir el programa
+# Ejecutar programas solamente (.exe)
+# no sirve para abrir archivos como tal
 def run_program(path: str) -> None:
-    pass
+    subprocess.run([path])
+
+
+def open_file_with_default_app(filepath: str) -> None:
+    subprocess.run(["start", "", filepath], shell=True, check=True)
 
 
 def volume_up(rate: int) -> None:
