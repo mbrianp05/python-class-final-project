2729c5866155a9f208c401ed260ec765dd4b4ed5
Brian <mbrianp05@gmail.com>
2026-04-20 11:46:24 -0400

    Improved names

--- gesture.py
+++ gesture.py
@@ -2,7 +2,11 @@ from dataclasses import dataclass
 from typing import Callable
 
 
+# Cuando el usuario configura un gesto como por ejemplo
+# abrir carpeta debe pasar indicar el path de la carpeta como un string
+# por ende se crea una instancia de Gesture[str] con "param" con el valor del "path"
 @dataclass(frozen=True)
-class Gesture:
+class Gesture[T]:
     name: str
-    execute: Callable[[], None]
+    trigger: Callable[[T], None]
+    param: T | None = None
--- services.py
+++ services.py
@@ -7,13 +7,13 @@ from gesture import Gesture
 # gestos reales con funciones que hagan algo
 def fetch_gestures() -> List[Gesture]:
     return [
-        Gesture(name="Abrir explorador", execute=lambda: None),
-        Gesture(name="Abrir editor", execute=lambda: None),
-        Gesture(name="Subir volumen", execute=lambda: None),
-        Gesture(name="Bajar volumen", execute=lambda: None),
-        Gesture(name="Encender wifi", execute=lambda: None),
-        Gesture(name="Apagar wifi", execute=lambda: None),
-        Gesture(name="Reproducir canción", execute=lambda: None),
-        Gesture(name="Pausar canción", execute=lambda: None),
-        Gesture(name="Abrir navegador", execute=lambda: None),
+        Gesture(name="Abrir explorador", trigger=lambda _: None),
+        Gesture(name="Abrir editor", trigger=lambda _: None),
+        Gesture(name="Subir volumen", trigger=lambda _: None),
+        Gesture(name="Bajar volumen", trigger=lambda _: None),
+        Gesture(name="Encender wifi", trigger=lambda _: None),
+        Gesture(name="Apagar wifi", trigger=lambda _: None),
+        Gesture(name="Reproducir canción", trigger=lambda _: None),
+        Gesture(name="Pausar canción", trigger=lambda _: None),
+        Gesture(name="Abrir navegador", trigger=lambda _: None),
     ]
