f209dd145f5767942c4786cd56f3320d5c89b35e
JavDCC <javierdavidcoroas@gmail.com>
2026-04-26 18:32:31 -0400

    Fixed Volume, added Wi-Fi and Screenshot actions.

--- actions.py
+++ actions.py
@@ -8,9 +8,13 @@
 # escribiendolo manualmente, copiandolo o con un botón para navegar de la UI
 # por el sistema de archivos hasta que llegue a la carpeta deseada
 
+import os
 import subprocess
+import asyncio
 
 from pycaw.pycaw import AudioUtilities
+from PIL import ImageGrab
+from winrt.windows.devices.radios import Radio, RadioKind, RadioState
 
 
 # abrir el explorador en esa carpeta
@@ -28,25 +32,65 @@ def open_file_with_default_app(filepath: str) -> None:
     subprocess.run(["start", "", filepath], shell=True, check=True)
 
 
-def set_volume(rate: float) -> None:
-    device = AudioUtilities.GetSpeakers()
-    volume = device.EndpointVolume  # type: ignore
-    max = 0
-    min = -21
-    new = min + (max - min) * rate
+def set_volume(level: int|float) -> None:
+    if(isinstance(level, int)):
+        level = max(0, min(level, 100))/100
+    elif(isinstance(level, float)):
+        level = max(0.0, min(level, 1.0))
+    else:
+        raise TypeError("Volume can be set to int (0 - 100) or float (0.0 - 1.0)")
 
-    print(int(new), max)
-
-    volume.SetMasterVolumeLevel(new, None)
-
-
-def wifi_on() -> None:
-    pass
+    device = AudioUtilities.GetSpeakers()           # Buscar el dispositivo
+    volume = device.EndpointVolume                  # Obtener la interfaz 
+    volume.SetMasterVolumeLevelScalar(level, None)  # Ajustar el volumen
 
 
-def wifi_off() -> None:
-    pass
+def set_wifi(state: bool) -> None:
+    # import asyncio
+    # from winrt.windows.devices.radios import Radio, RadioKind, RadioState
+
+    state = (RadioState.ON if state else RadioState.OFF)
+
+    async def async_set_wifi() -> None:
+        try:
+            radios = await Radio.get_radios_async() # Buscar todos los dispositivos
+
+            wifi_radio = None
+
+            for i in radios:                    # Encontrar el primer dispositivo Wi-Fi
+                if i.kind == RadioKind.WI_FI:
+                    wifi_radio = i
+                    break
+
+            if wifi_radio is None:
+                print("No Wi-Fi was found")         # Mensaje provisional
+
+            if wifi_radio.state != state:           # Actualizar el estado
+                print("Toggling Wi-Fi")             # Mensaje provisional
+                result = await wifi_radio.set_state_async(state)
+
+        except Exception as e:
+            print(f"ERROR - {e}")       # Captura provisional
+
+    asyncio.run(async_set_wifi())
+
+    print(f"WiFi turned {"on" if state == RadioState.ON else "off"}")    # Mensaje provisional
 
 
-def take_screenshot() -> None:
-    pass
+def take_screenshot(name="Test.png", path="Screenshots") -> None:
+    # import os
+
+    try:
+        screenshot = ImageGrab.grab()                       # Captura de pantalla
+        if(not os.path.exists(path)): os.makedirs(path)     # Crear la carpeta si no existe
+        screenshot.save(path + "/" + name)                  # Guardar la captura
+        print(f"Screenshot saved at {path}/{name}")         # Mensaje provisional
+    except Exception as e:
+        print(f"ERROR - {e}")       # Captura provisional
+
+# set_volume(0)
+# set_volume(0.5)
+# set_volume(100)
+# take_screenshot()
+# set_wifi(True)
+# set_wifi(False)
