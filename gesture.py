1813603e2260d09c4cc3e5c6e95d25555c03b17a
Brian <mbrianp05@gmail.com>
2026-04-30 11:04:33 -0400

    Preps for gesture recognition

--- gesture.py
+++ gesture.py
@@ -1,5 +1,7 @@
 from dataclasses import dataclass
-from typing import Callable
+from typing import Callable, Dict, List, Literal, Tuple
+
+from utilityclasses import Finger
 
 
 # Cuando el usuario configura un gesto como por ejemplo
@@ -8,5 +10,22 @@ from typing import Callable
 @dataclass(frozen=True)
 class Gesture[T]:
     name: str
-    trigger: Callable[[T], None]
-    param: T | None = None
+    trigger: Callable[[List[T]], None]
+    params: List[T]
+
+
+# En esta clase se guarda toda la data
+# relevante que vamos a utilizar para los gestos
+# como el numero de manos que aparecen,
+# la cantidad de dedos levantados, etc.
+class GestureData:
+    hands: Literal[0, 1, 2]
+    visibleFingers: Tuple[Dict[Finger, bool], Dict[Finger, bool]]
+
+
+# Esta clase se encargara de manejar la logica detectar un gesto
+# de forma generica, luego se comparara la data del gesto que el usuario hace
+# con la data de los gestos configurados
+class GestureRecognition:
+    def __init__(self):
+        pass
---
+++ utilityclasses.py
@@ -0,0 +1,8 @@
+from enum import StrEnum
+
+
+class Finger(StrEnum):
+    THUMB = "thumb"
+    FOREFINGER = "forefinger"
+    MIDDLE_FINGER = "middle_finger"
+    LITTLE_FINGER = "little_finger"
