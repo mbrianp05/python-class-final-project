43d8fb54f019804c1d4c648ed1cd41de11157524
Brian <mbrianp05@gmail.com>
2026-05-01 10:59:34 -0400

    Updated predefined gestures

--- services.py
+++ services.py
@@ -7,13 +7,13 @@ from gesture import Gesture
 # gestos reales con funciones que hagan algo
 def fetch_gestures() -> List[Gesture]:
     return [
-        Gesture(name="Abrir explorador", trigger=lambda _: None),
-        Gesture(name="Abrir editor", trigger=lambda _: None),
-        Gesture(name="Subir volumen", trigger=lambda _: None),
-        Gesture(name="Bajar volumen", trigger=lambda _: None),
-        Gesture(name="Encender wifi", trigger=lambda _: None),
-        Gesture(name="Apagar wifi", trigger=lambda _: None),
-        Gesture(name="Reproducir canción", trigger=lambda _: None),
-        Gesture(name="Pausar canción", trigger=lambda _: None),
-        Gesture(name="Abrir navegador", trigger=lambda _: None),
+        Gesture(name="Abrir explorador", trigger=lambda _: None, params=[]),
+        Gesture(name="Abrir editor", trigger=lambda _: None, params=[]),
+        Gesture(name="Subir volumen", trigger=lambda _: None, params=[]),
+        Gesture(name="Bajar volumen", trigger=lambda _: None, params=[]),
+        Gesture(name="Encender wifi", trigger=lambda _: None, params=[]),
+        Gesture(name="Apagar wifi", trigger=lambda _: None, params=[]),
+        Gesture(name="Reproducir canción", trigger=lambda _: None, params=[]),
+        Gesture(name="Pausar canción", trigger=lambda _: None, params=[]),
+        Gesture(name="Abrir navegador", trigger=lambda _: None, params=[]),
     ]
