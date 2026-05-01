697461386908d828fea99784c38a22f1b91068d7
Brian <mbrianp05@gmail.com>
2026-05-01 11:28:59 -0400

    Implemented Finger enum

--- utilityclasses.py
+++ utilityclasses.py
@@ -1,8 +1,11 @@
 from enum import StrEnum
 
+# AQUI IRAN TODAS LAS CLASES RELACIONADAS CON LA LOGICA
+
 
 class Finger(StrEnum):
-    THUMB = "thumb"
-    FOREFINGER = "forefinger"
+    THUMB_FINGER = "thumb"
+    INDEX_FINGER = "index_finger"
     MIDDLE_FINGER = "middle_finger"
+    RING_FINGER = "ring_finger"
     LITTLE_FINGER = "little_finger"
