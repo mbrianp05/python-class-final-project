cc6aaa2af6883249326eca3549b735a386228e8a
Brian <mbrianp05@gmail.com>
2026-05-02 13:42:12 -0400

    Add utility class to easy the impl of gesture analysis

--- utilityclasses.py
+++ utilityclasses.py
@@ -9,3 +9,12 @@ class Finger(StrEnum):
     MIDDLE_FINGER = "middle_finger"
     RING_FINGER = "ring_finger"
     LITTLE_FINGER = "little_finger"
+
+
+# Para saber la "orientación" de la mano
+# Empezaremos por estas pero se puede incrementar
+# para detectar gestos con la parte lateral de la mano
+# o con el puño orientado a la camara, etc.
+class HandProfile(StrEnum):
+    PALM = "palm"
+    FRONT = "front"
