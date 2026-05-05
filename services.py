357e83d50b3d70bfd547566584c57b0d057e15a1
Brian <mbrianp05@gmail.com>
2026-05-05 12:34:39 -0400

    Improved mocked Gestures service

--- services.py
+++ services.py
@@ -1,19 +1,45 @@
 from typing import List
 
-from gesture import Gesture
+from gesture import Gesture, GestureData
+
+
+def mock_gesturedata() -> GestureData:
+    return GestureData(
+        hands=0,
+        visibleFingers=([], []),
+    )
 
 
 # Gestos de prueba de momento en el futuro sería
 # gestos reales con funciones que hagan algo
 def fetch_gestures() -> List[Gesture]:
+    data = mock_gesturedata()
+
     return [
-        Gesture(name="Abrir explorador", trigger=lambda _: None, params=[]),
-        Gesture(name="Abrir editor", trigger=lambda _: None, params=[]),
-        Gesture(name="Subir volumen", trigger=lambda _: None, params=[]),
-        Gesture(name="Bajar volumen", trigger=lambda _: None, params=[]),
-        Gesture(name="Encender wifi", trigger=lambda _: None, params=[]),
-        Gesture(name="Apagar wifi", trigger=lambda _: None, params=[]),
-        Gesture(name="Reproducir canción", trigger=lambda _: None, params=[]),
-        Gesture(name="Pausar canción", trigger=lambda _: None, params=[]),
-        Gesture(name="Abrir navegador", trigger=lambda _: None, params=[]),
+        Gesture(
+            name="Abrir explorador",
+            trigger=lambda _: None,
+            param=None,
+            settings=data,
+        ),
+        Gesture(name="Abrir editor", trigger=lambda _: None, param=None, settings=data),
+        Gesture(
+            name="Subir volumen", trigger=lambda _: None, param=None, settings=data
+        ),
+        Gesture(
+            name="Bajar volumen", trigger=lambda _: None, param=None, settings=data
+        ),
+        Gesture(
+            name="Encender wifi", trigger=lambda _: None, param=None, settings=data
+        ),
+        Gesture(name="Apagar wifi", trigger=lambda _: None, param=None, settings=data),
+        Gesture(
+            name="Reproducir canción", trigger=lambda _: None, param=None, settings=data
+        ),
+        Gesture(
+            name="Pausar canción", trigger=lambda _: None, param=None, settings=data
+        ),
+        Gesture(
+            name="Abrir navegador", trigger=lambda _: None, param=None, settings=data
+        ),
     ]
