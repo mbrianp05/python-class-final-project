f49c4a7bca5e6ae12eb1c600bcb8b8a49f75f0ca
Brian <mbrianp05@gmail.com>
2026-05-15 22:42:04 -0400

    Added detection for duplacted IDs

--- services.py
+++ services.py
@@ -2,33 +2,54 @@ import json
 from typing import List
 
 import utils
-from gesture import Gesture, GestureData, HandProfile
-from utilityclasses import Finger
-
-
-def mock_gesturedata() -> GestureData:
-    return GestureData(
-        hands=(True, True),
-        visibleFingers=(
-            [Finger.THUMB_FINGER, Finger.INDEX_FINGER, Finger.LITTLE_FINGER],
-            [],
-        ),
-        profile=(None, HandProfile.PALM),
-    )
+from gesture import Gesture
 
 
 # Añadir un gesto más a los que ya hay en el json
-def persist_gesture(gesture: Gesture):
-    persist_gestures([gesture])
+def persist_gesture(gesture: Gesture) -> bool:
+    return persist_gestures([gesture])
 
 
 # Añadir varios gestos a la vez manteniendo los que ya hay en el json
-def persist_gestures(gestures: List[Gesture]):
+def persist_gestures(gestures: List[Gesture]) -> bool:
     all_gestures = gestures + fetch_gestures()
+    ids = [g.id for g in all_gestures]
+    ids_set = set(ids)
+
+    # Existen ids repetidas
+    if len(ids) != len(ids_set):
+        return False
+
     serializable = [utils.turn_gesture_into_dict(g) for g in all_gestures]
 
     with open(utils.CONFIG_FILE_NAME, "w") as config_file:
         json.dump(serializable, config_file, indent=4)
+
+    return True
+
+
+def update_gesture(gesture: Gesture) -> bool:
+    list = fetch_gestures()
+    old = None
+    old_index = -1
+
+    for i, g in enumerate(list):
+        if g.id == gesture.id:
+            old = g
+            old_index = i
+
+    if old is None:
+        return False
+
+    list.remove(old)
+    list.insert(old_index, gesture)
+
+    serializable = [utils.turn_gesture_into_dict(g) for g in list]
+
+    with open(utils.CONFIG_FILE_NAME, "w") as config_file:
+        json.dump(serializable, config_file, indent=4)
+
+    return True
 
 
 # Gestos de prueba de momento en el futuro sería
